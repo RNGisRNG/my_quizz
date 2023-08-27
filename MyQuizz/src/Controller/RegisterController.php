88f8ace574e24de249e01d9499de9eefc5404042
Cem HOROZOGLU <cem.horozoglu@epitech.eu>
2023-08-27 16:29:00 +0200

    Register, controller, form register

---
+++ MyQuizz/src/Controller/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('register/index.html.twig', [
+            'user' => $user,
+            'form' => $form,
+            'connected' => false,
+        ]);
+    }
+}
